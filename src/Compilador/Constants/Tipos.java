af39bd12ef20d03a22a67840041d8b4096baba6b
Tuane Quintella <q.tuane@gmail.com>
2011-11-08 12:26:21 -0200

    Definidos tipos de funcoes

--- src/Compilador/Constants/Tipos.java
+++ src/Compilador/Constants/Tipos.java
@@ -7,5 +7,9 @@ public class Tipos {
     public static final int Variavel = 1;
     public static final int Inteiro = 2;
     public static final int Booleano = 3;
+    public static final int FuncaoInteiro = 4;
+    public static final int FuncaoBooleano = 5;
+    public static final int Funcao = 6;
+    public static final int Procedimento = 7;
     
 }
