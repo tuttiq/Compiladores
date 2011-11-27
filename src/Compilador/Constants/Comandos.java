bf79cdfc9752b0657840ed10ce864e500fde9311
Tuane Quintella <q.tuane@gmail.com>
2011-11-27 15:37:55 -0200

    Adicionados comandos da VM para geracao de codigo

---
+++ src/Compilador/Constants/Comandos.java
@@ -0,0 +1,35 @@
+
+package Compilador.Constants;
+
+public class Comandos {
+
+    public static final String Start = "START";
+    public static final String Allocate = "ALLOC";
+    public static final String Deallocate = "DALLOC";
+    public static final String Halt = "HLT";
+    public static final String Label = "L";
+    public static final String LoadConst = "LDC";
+    public static final String LoadValue = "LDV";
+    public static final String Add = "ADD";
+    public static final String Subtract = "SUB";
+    public static final String Multiply = "MULT";
+    public static final String Divide = "DIVI";
+    public static final String Invert = "INV";
+    public static final String And = "AND";
+    public static final String Or = "OR";
+    public static final String Negate = "NEG";
+    public static final String CmpLower = "CME";
+    public static final String CmpHigher = "CMA";
+    public static final String CmpEqual = "CEQ";
+    public static final String CmpDifferent = "CDIF";
+    public static final String CmpLowerEqual = "CMEQ";
+    public static final String CmpHigherEqual = "CMAQ";
+    public static final String Store = "STR";
+    public static final String Jump = "JMP";
+    public static final String JumpIfFalse = "JMPF";
+    public static final String Null = "NULL";
+    public static final String Read = "RD";
+    public static final String Print = "PRN";
+    public static final String Call = "CALL";
+    public static final String Return = "RETURN";
+}
